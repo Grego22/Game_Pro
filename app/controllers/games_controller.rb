a880a5b17792fe21a639e670be7c05654859eba7
Greg Garville Jr <gjgarvil@gmail.com>
2017-11-09 10:54:12 -0500

    added befored action for private method find_games in game controller, generated show.html.erb and coded placeholders

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,9 @@
 class GamesController < ApplicationController
+  before_action :find_game, only: [:show, :update, :edit, :destroy]
   def index
     @games = Game.all.order("created_at DESC")
+  end
+  def show
   end
   def new
     @game = Game.new
@@ -20,5 +23,8 @@ class GamesController < ApplicationController
 
   def game_params
     params.require(:game).permit(:title, :description, :company)
+  end
+  def find_game
+    @game = Game.find(params[:id])
   end
 end
