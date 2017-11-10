cf930d09d12680f267df34629465404f3ddc12a5
Greg Garville Jr <gjgarvil@gmail.com>
2017-11-10 03:07:24 -0500

    added catergory methods to games controller, seed catergory from temrinal, and added H4 to show.html

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,9 +7,11 @@ class GamesController < ApplicationController
   end
   def new
     @game = current_user.games.build
+    @catergories = Catergory.all.map{ |c| [c.name, c.id]}
   end
   def create
     @game = current_user.games.build(game_params)
+    @game.catergory_id = params[:catergory_id]
     if @game.save
       redirect_to root_path
     else
@@ -19,9 +21,12 @@ class GamesController < ApplicationController
   def show
   end
   def edit
+    @catergories = Catergory.all.map{ |c| [c.name, c.id]}
   end
 
   def update
+    @catergories = Catergory.all.map{ |c| [c.name, c.id]}
+    
     if @game.update(game_params)
       redirect_to game_path(@game)
     else
@@ -34,7 +39,7 @@ class GamesController < ApplicationController
   end
 
   def game_params
-    params.require(:game).permit(:title, :description, :company)
+    params.require(:game).permit(:title, :description, :company, :catergory_id)
   end
   def find_game
     @game = Game.find(params[:id])
