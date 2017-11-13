67673e8e3fb77c224a47c87bd977e329a1ce80e1
Greg Garville Jr <gjgarvil@gmail.com>
2017-11-13 15:50:45 -0500

    added game_id migration and Model Associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   belongs_to :user
   belongs_to :catergory
+  has_many   :reviews
+
   has_attached_file :img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
 end
